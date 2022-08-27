9dc3f030280e1a15112b388fab272f0e1a4f3dec
gsapijaszko <grzegorz@sapijaszko.net>
2022-08-27 22:29:54 +0200

    shops, etc

--- README.md
+++ README.md
@@ -2,16 +2,7 @@
  
 Bike map based on OSM data
 
-Using the map since a while (the earliest print out I have found are dated 2017), so I thought it can be useful to others. It's mainly based on [OpenStreetMap](https://www.openstreetmap.org/) data (you can download it from [Geofabrik](https://download.geofabrik.de/)), uses postgres, postgis and QGIS. The main purpose of the map is to be printed as atlas on A4 paper sheets (you can find QGIS Atlas features in project as well).
-
-For Poland, where I live, I'm using additional online WMS/WFS:
-    
-* raster relief data from [geoportal.gov.pl](https://www.geoportal.gov.pl/uslugi/usluga-przegladania-wms) as background
-
-* vector boundary data for nature reserves, parks etc from [GDOŚ](https://sdi.gdos.gov.pl/wfs?request=GetCapabilities&service=WFS)
-    
-
-  * WMS raster with forest areas where you can stay overnight in the wilderness [Lasy Państwowe](https://www.bdl.lasy.gov.pl/portal/)
+Using this kind of map since a while (the earliest print out I have found are dated 2017), so I thought it can be useful to others. It's mainly based on [OpenStreetMap](https://www.openstreetmap.org/) data (you can download it from [Geofabrik](https://download.geofabrik.de/)), uses `postgres`, `postgis` and `QGIS`. The main purpose of the map is to be printed as atlas on A4 paper sheets (you can find QGIS Atlas features in project as well). The base scale is 1:50 000.
 
 How it looks like you can see on the screenshot below.
 
@@ -22,12 +13,58 @@ How it looks like you can see on the screenshot below.
 
 To use the OSM data we will use `osm2pgsql`[^1] with a slighty adopted style (added in osm2pgsql folder):
 
-    osm2pgsql -c -U postgres -W -d osmdb --slim --hstore-all --style ~/osm2pgsql/default.style --multi-geometry --merc ~/Downloads/poland-latest.osm.pbf
+    osm2pgsql -c -U postgres -W -d osmdb --slim --hstore-all --style default.style --multi-geometry --merc poland-latest.osm.pbf
 
-Then we have to run qsl script with views (stored in `sql/views.sql' file), which will be used in QGIS:
+Then we have to run qsl script to create neccessary function(s) and views (stored in `sql/views.sql')
 
     psql -U postgres -W -d osmdb -f views.sql
 
-[^1]: [osm2pgsql](https://osm2pgsql.org/) 
+
+For Poland, where I live, I'm using additional data sources:
+
+* raster relief data from [geoportal.gov.pl](https://www.geoportal.gov.pl/uslugi/usluga-przegladania-wms) as background;
+
+* vector boundary data for nature reserves, parks etc from [GDOŚ](https://sdi.gdos.gov.pl/wfs?request=GetCapabilities&service=WFS);
+
+* WMS raster with forest areas where you can stay overnight in the wilderness [Lasy Państwowe](https://www.bdl.lasy.gov.pl/portal/);
+
+
+## Data use, styling and reasons behind it
+
+If I recall it correctly, it somewhat started with Anita's Graser post about creation of Google like maps with OSM data [^2]. Since then it went through a lot of iterations, styles changes, playing with icons, colors and details.
+
+The main reason behind the map is to have it printed out on hand, during bike trips. Hence such design assumptions: 
+
+* must have:
+    * cycle and foot routes
+    * surface of the highways of the routes --- there is no pleasure to cycle with panniers over paving stones or sand :)
+    * shelters, picnic sites, fire pits
+    * bike shops / services and bike repair stations
+    * stores and groceries, especially in rural areas
+    * railways and railway station
+
+* nice to have:
+    * hotels, motels and other resting options
+    * tourist attractions
+
+### Landuse, nature reserve - polygons
+
+The background of the map consist of `landuse` taken from `planet_osm_polygon` table. You can find details in `bikemap_landuse`, `bikemap_water` and `bikemap_resind` views. Styling is not sofisticated, however for shrubs (and probably wetlands) I'm using some symbols prepared by Yury Ryabov and Pavel Sergeev [^3]. 
+
+Over it we have few layers from GDOŚ with different types of nature reserve areas. If you would like to extract the data from OSM, please have a look on [`leisure=nature_reserve`](https://wiki.openstreetmap.org/wiki/Tag:leisure%3Dnature_reserve) and [`protect_class`](https://wiki.openstreetmap.org/wiki/Key:protect_class) keys.
+
+
+## To do
+
+-- exclude shops from the city areas? first check the assumption, if all city areas (admin_level = 8) has stores
+
+-- simplify railways and multilane highways (https://gis.stackexchange.com/questions/319412/simplifying-multiple-lines-to-create-central-axis)
+
+-- transpose railway stations on railway
+
 
 ## Footnotes
+
+[^1]: [osm2pgsql](https://osm2pgsql.org/)
+[^2]: see [anitagraser.com](https://anitagraser.com/2014/05/31/a-guide-to-googlemaps-like-maps-with-osm-in-qgis/)
+[^3]: some symbols come from QGIS sybology set made by Yury Ryabov and Pavel Sergeev, available at [https://ssrebelious.wordpress.com/2011/10/11/qgis-symbology-set/](https://ssrebelious.wordpress.com/2011/10/11/qgis-symbology-set/)
--- sql/views.sql
+++ sql/views.sql
@@ -1,6 +1,6 @@
 -- FUNCTION: public.bikemap_findangle(geometry)
 
-DROP FUNCTION IF EXISTS public.bikemap_findangle(geometry);
+-- DROP FUNCTION IF EXISTS public.bikemap_findangle(geometry);
 
 CREATE OR REPLACE FUNCTION public.bikemap_findangle(
 	point geometry)
@@ -32,16 +32,107 @@ SELECT ST_ClosestPoint(way, testPoint) as cp, way
 	) AS dupa1);
 
 RETURN QUERY SELECT ST_Azimuth(ST_StartPoint(myWay), ST_EndPoint(myWay))/(2*pi())*360;
+-- RETURN;
 End;
 $BODY$;
 
 ALTER FUNCTION public.bikemap_findangle(geometry)
     OWNER TO postgres;
 
--------
--------
+
+-- View: public.bikemap_bicycle_rest_areas
+
+DROP MATERIALIZED VIEW IF EXISTS public.bikemap_bicycle_rest_areas;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS public.bikemap_bicycle_rest_areas
+TABLESPACE pg_default
+AS
+ SELECT planet_osm_point.osm_id,
+    planet_osm_point.name,
+    planet_osm_point.way
+   FROM planet_osm_point
+  WHERE planet_osm_point.bicycle = 'designated'::text AND planet_osm_point.highway = 'rest_area'::text
+UNION
+ SELECT planet_osm_line.osm_id,
+    planet_osm_line.name,
+    st_centroid(planet_osm_line.way)::geometry(Point,3857) AS way
+   FROM planet_osm_line
+  WHERE planet_osm_line.bicycle = 'designated'::text AND planet_osm_line.highway = 'rest_area'::text
+UNION
+ SELECT planet_osm_polygon.osm_id,
+    planet_osm_polygon.name,
+    st_centroid(planet_osm_polygon.way)::geometry(Point,3857) AS way
+   FROM planet_osm_polygon
+  WHERE planet_osm_polygon.bicycle = 'designated'::text AND planet_osm_polygon.highway = 'rest_area'::text
+WITH DATA;
+
+ALTER TABLE IF EXISTS public.bikemap_bicycle_rest_areas
+    OWNER TO postgres;
 
 
+CREATE INDEX planet_bikemap_bicycle_rest_areas_way_idx
+    ON public.bikemap_bicycle_rest_areas USING gist
+    (way)
+    TABLESPACE pg_default;
+	
+	
+-- View: public.bikemap_bicycle_service_and_shop
+
+DROP MATERIALIZED VIEW IF EXISTS public.bikemap_bicycle_service_and_shop;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS public.bikemap_bicycle_service_and_shop
+TABLESPACE pg_default
+AS
+ SELECT row_number() OVER () AS id,
+    bicycle_service.way::geometry(Point,3857) AS way,
+    bicycle_service.type
+   FROM ( SELECT st_centroid(unnest(st_clusterwithin(aaa.way, 1000::double precision))) AS way,
+            aaa.type
+           FROM ( SELECT planet_osm_point.way,
+                        CASE
+                            WHEN planet_osm_point.tags @> '"service:bicycle:repair"=>"yes"'::hstore AND planet_osm_point.shop = 'bicycle'::text THEN 'service'::character varying
+                            WHEN planet_osm_point.amenity = 'bicycle_repair_station'::text THEN 'station'::character varying
+                            WHEN planet_osm_point.shop = 'bicycle'::text THEN 'shop'::character varying
+                            ELSE NULL::character varying
+                        END AS type
+                   FROM planet_osm_point
+                  WHERE planet_osm_point.amenity = 'bicycle_repair_station'::text OR planet_osm_point.shop = 'bicycle'::text
+                UNION
+                 SELECT st_centroid(planet_osm_line.way) AS way,
+                        CASE
+                            WHEN planet_osm_line.tags @> '"service:bicycle:repair"=>"yes"'::hstore AND planet_osm_line.shop = 'bicycle'::text THEN 'service'::character varying
+                            WHEN planet_osm_line.amenity = 'bicycle_repair_station'::text THEN 'station'::character varying
+                            WHEN planet_osm_line.shop = 'bicycle'::text THEN 'shop'::character varying
+                            ELSE NULL::character varying
+                        END AS type
+                   FROM planet_osm_line
+                  WHERE planet_osm_line.amenity = 'bicycle_repair_station'::text OR planet_osm_line.shop = 'bicycle'::text
+                UNION
+                 SELECT st_centroid(planet_osm_polygon.way) AS way,
+                        CASE
+                            WHEN planet_osm_polygon.tags @> '"service:bicycle:repair"=>"yes"'::hstore AND planet_osm_polygon.shop = 'bicycle'::text THEN 'service'::character varying
+                            WHEN planet_osm_polygon.amenity = 'bicycle_repair_station'::text THEN 'station'::character varying
+                            WHEN planet_osm_polygon.shop = 'bicycle'::text THEN 'shop'::character varying
+                            ELSE NULL::character varying
+                        END AS type
+                   FROM planet_osm_polygon
+                  WHERE planet_osm_polygon.amenity = 'bicycle_repair_station'::text OR planet_osm_polygon.shop = 'bicycle'::text) aaa
+          GROUP BY aaa.type) bicycle_service
+WITH DATA;
+
+ALTER TABLE IF EXISTS public.bikemap_bicycle_service_and_shop
+    OWNER TO postgres;
+
+
+CREATE INDEX bikemap_bicycle_service_and_shop_type
+    ON public.bikemap_bicycle_service_and_shop USING btree
+    (type COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
+CREATE INDEX bikemap_bicycle_service_and_shop_way_idx
+    ON public.bikemap_bicycle_service_and_shop USING gist
+    (way)
+    TABLESPACE pg_default;
+	
 -- View: public.bikemap_embankments_cuttings
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_embankments_cuttings;
@@ -75,7 +166,7 @@ CREATE INDEX bikemap_embankments_way_idx
     ON public.bikemap_embankments_cuttings USING gist
     (way)
     TABLESPACE pg_default;
-
+	
 -- View: public.bikemap_ferries
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_ferries;
@@ -100,7 +191,8 @@ CREATE INDEX bikemap_ferries_way_idx
     ON public.bikemap_ferries USING gist
     (way)
     TABLESPACE pg_default;
-	
+
+
 -- View: public.bikemap_highways
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_highways;
@@ -111,16 +203,20 @@ AS
  SELECT row_number() OVER () AS id,
     highways.ref,
     highways.highway,
-    highways.geom::geometry(MultiLineString,3857) AS geom
+    highways.tracktype,
+    highways.geom::geometry(MultiLineString,3857) AS geom,
+    st_length(highways.geom::geometry(MultiLineString,3857)) AS length
    FROM ( SELECT planet_osm_line.ref,
             planet_osm_line.highway,
+            planet_osm_line.tracktype,
             st_multi(st_union(planet_osm_line.way)) AS geom
            FROM planet_osm_line
           WHERE planet_osm_line.highway IS NOT NULL AND planet_osm_line.ref IS NOT NULL
-          GROUP BY planet_osm_line.highway, planet_osm_line.ref
+          GROUP BY planet_osm_line.highway, planet_osm_line.ref, planet_osm_line.tracktype
         UNION
          SELECT ''::text AS ref,
             planet_osm_line.highway,
+            planet_osm_line.tracktype,
             st_multi(planet_osm_line.way) AS geom
            FROM planet_osm_line
           WHERE planet_osm_line.highway IS NOT NULL AND planet_osm_line.ref IS NULL) highways
@@ -134,11 +230,95 @@ CREATE INDEX bikemap_highways_geom_idx
     ON public.bikemap_highways USING gist
     (geom)
     TABLESPACE pg_default;
-CREATE UNIQUE INDEX bikemap_highways_id
+CREATE INDEX bikemap_highways_highway_tracktype_idx
     ON public.bikemap_highways USING btree
-    (id)
+    (highway COLLATE pg_catalog."default", tracktype COLLATE pg_catalog."default")
     TABLESPACE pg_default;
 	
+
+
+-- View: public.bikemap_hotels
+
+DROP MATERIALIZED VIEW IF EXISTS public.bikemap_hotels;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS public.bikemap_hotels
+TABLESPACE pg_default
+AS
+ SELECT row_number() OVER () AS id,
+    hotels.myway::geometry(Point,3857) AS way,
+    hotels.tourism,
+    hotels.name,
+    hotels.address,
+    hotels.email,
+    hotels.phone
+   FROM ( WITH table1 AS (
+                 SELECT st_centroid(unnest(st_clusterwithin(aaa.way, 1000::double precision))) AS myway,
+                    aaa.tourism
+                   FROM ( SELECT planet_osm_point.tourism,
+                            planet_osm_point.way
+                           FROM planet_osm_point
+                          WHERE planet_osm_point.tourism = ANY (ARRAY['camp_site'::text, 'hostel'::text, 'apartment'::text, 'guest_house'::text, 'chalet'::text, 'alpine_hut'::text, 'caravan_site'::text, 'hotel'::text, 'motel'::text])
+                        UNION
+                         SELECT planet_osm_line.tourism,
+                            st_centroid(planet_osm_line.way) AS way
+                           FROM planet_osm_line
+                          WHERE planet_osm_line.tourism = ANY (ARRAY['camp_site'::text, 'hostel'::text, 'apartment'::text, 'guest_house'::text, 'chalet'::text, 'alpine_hut'::text, 'caravan_site'::text, 'hotel'::text, 'motel'::text])
+                        UNION
+                         SELECT planet_osm_polygon.tourism,
+                            st_centroid(planet_osm_polygon.way) AS way
+                           FROM planet_osm_polygon
+                          WHERE planet_osm_polygon.tourism = ANY (ARRAY['camp_site'::text, 'hostel'::text, 'apartment'::text, 'guest_house'::text, 'chalet'::text, 'alpine_hut'::text, 'caravan_site'::text, 'hotel'::text, 'motel'::text])) aaa
+                  GROUP BY aaa.tourism
+                ), table2 AS (
+                 SELECT planet_osm_point.way,
+                    planet_osm_point.name,
+                    concat(planet_osm_point.tags -> 'addr:street'::text, ' ', planet_osm_point.tags -> 'addr:housenumber'::text, ', ', planet_osm_point.tags -> 'addr:city'::text) AS address,
+                    planet_osm_point.tags -> 'contact:email'::text AS email,
+                    planet_osm_point.tags -> 'contact:phone'::text AS phone
+                   FROM planet_osm_point
+                  WHERE planet_osm_point.tourism = ANY (ARRAY['camp_site'::text, 'hostel'::text, 'apartment'::text, 'guest_house'::text, 'chalet'::text, 'alpine_hut'::text, 'caravan_site'::text, 'hotel'::text, 'motel'::text])
+                UNION
+                 SELECT st_centroid(planet_osm_line.way) AS way,
+                    planet_osm_line.name,
+                    concat(planet_osm_line.tags -> 'addr:street'::text, ' ', planet_osm_line.tags -> 'addr:housenumber'::text, ', ', planet_osm_line.tags -> 'addr:city'::text) AS address,
+                    planet_osm_line.tags -> 'contact:email'::text AS email,
+                    planet_osm_line.tags -> 'contact:phone'::text AS phone
+                   FROM planet_osm_line
+                  WHERE planet_osm_line.tourism = ANY (ARRAY['camp_site'::text, 'hostel'::text, 'apartment'::text, 'guest_house'::text, 'chalet'::text, 'alpine_hut'::text, 'caravan_site'::text, 'hotel'::text, 'motel'::text])
+                UNION
+                 SELECT st_centroid(planet_osm_polygon.way) AS way,
+                    planet_osm_polygon.name,
+                    concat(planet_osm_polygon.tags -> 'addr:street'::text, ' ', planet_osm_polygon.tags -> 'addr:housenumber'::text, ', ', planet_osm_polygon.tags -> 'addr:city'::text) AS address,
+                    planet_osm_polygon.tags -> 'contact:email'::text AS email,
+                    planet_osm_polygon.tags -> 'contact:phone'::text AS phone
+                   FROM planet_osm_polygon
+                  WHERE planet_osm_polygon.tourism = ANY (ARRAY['camp_site'::text, 'hostel'::text, 'apartment'::text, 'guest_house'::text, 'chalet'::text, 'alpine_hut'::text, 'caravan_site'::text, 'hotel'::text, 'motel'::text])
+                )
+         SELECT table1.myway,
+            table1.tourism,
+            table2.way,
+            table2.name,
+            table2.address,
+            table2.email,
+            table2.phone
+           FROM table1
+             LEFT JOIN table2 ON st_intersects(table1.myway, table2.way)) hotels
+WITH DATA;
+
+ALTER TABLE IF EXISTS public.bikemap_hotels
+    OWNER TO postgres;
+
+
+CREATE INDEX bikemap_hotels_tourism_idx
+    ON public.bikemap_hotels USING btree
+    (tourism COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
+CREATE INDEX bikemap_hotels_way_idx
+    ON public.bikemap_hotels USING gist
+    (way)
+    TABLESPACE pg_default;
+	
+
 -- View: public.bikemap_landuse
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_landuse;
@@ -246,7 +426,7 @@ CREATE INDEX bikemap_man_made_center_idx
     ON public.bikemap_man_made USING gist
     (center)
     TABLESPACE pg_default;
-	
+
 -- View: public.bikemap_places
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_places;
@@ -317,7 +497,7 @@ CREATE INDEX planet_osm_railway_stations_center_idx
     ON public.bikemap_railway_stations USING gist
     (center)
     TABLESPACE pg_default;
-	
+
 -- View: public.bikemap_railways
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_railways;
@@ -376,6 +556,7 @@ WITH DATA;
 ALTER TABLE IF EXISTS public.bikemap_resind
     OWNER TO postgres;
 
+
 CREATE INDEX bikemap_resind_landuse_idx
     ON public.bikemap_resind USING btree
     (landuse COLLATE pg_catalog."default")
@@ -383,8 +564,8 @@ CREATE INDEX bikemap_resind_landuse_idx
 CREATE INDEX bikemap_resind_way_idx
     ON public.bikemap_resind USING gist
     (way)
-    TABLESPACE pg_default;	
-
+    TABLESPACE pg_default;
+	
 -- View: public.bikemap_routes
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_routes;
@@ -425,7 +606,7 @@ CREATE INDEX bikemap_routes_way_idx
     ON public.bikemap_routes USING gist
     (way)
     TABLESPACE pg_default;
-
+	
 -- View: public.bikemap_routes_surface
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_routes_surface;
@@ -454,7 +635,8 @@ CREATE INDEX bikemap_routes_surface_way_idx
     ON public.bikemap_routes_surface USING gist
     (way)
     TABLESPACE pg_default;
-	
+
+
 -- View: public.bikemap_shelters
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_shelters;
@@ -471,7 +653,7 @@ AS
                         CASE
                             WHEN planet_osm_point.leisure = 'firepit'::text THEN 'firepit'::text
                             WHEN planet_osm_point.amenity = 'bicycle_parking'::text THEN 'bicycle_parking'::text
-                            WHEN planet_osm_point.amenity = 'shelter'::text OR planet_osm_point.tourism = 'alpine_hut'::text OR planet_osm_point.highway = 'bus_stop'::text AND planet_osm_point.tags @> '"shelter"=>"yes"'::hstore THEN 'shelter'::text
+                            WHEN planet_osm_point.amenity = 'shelter'::text OR planet_osm_point.highway = 'bus_stop'::text AND planet_osm_point.tags @> '"shelter"=>"yes"'::hstore THEN 'shelter'::text
                             WHEN planet_osm_point.tourism = 'picnic_site'::text THEN 'picnic_site'::text
                             ELSE NULL::text
                         END AS type
@@ -482,7 +664,7 @@ AS
                         CASE
                             WHEN planet_osm_line.leisure = 'firepit'::text THEN 'firepit'::text
                             WHEN planet_osm_line.amenity = 'bicycle_parking'::text THEN 'bicycle_parking'::text
-                            WHEN planet_osm_line.amenity = 'shelter'::text OR planet_osm_line.tourism = 'alpine_hut'::text OR planet_osm_line.highway = 'bus_stop'::text AND planet_osm_line.tags @> '"shelter"=>"yes"'::hstore THEN 'shelter'::text
+                            WHEN planet_osm_line.amenity = 'shelter'::text OR planet_osm_line.highway = 'bus_stop'::text AND planet_osm_line.tags @> '"shelter"=>"yes"'::hstore THEN 'shelter'::text
                             WHEN planet_osm_line.tourism = 'picnic_site'::text THEN 'picnic_site'::text
                             ELSE NULL::text
                         END AS type
@@ -493,7 +675,7 @@ AS
                         CASE
                             WHEN planet_osm_polygon.leisure = 'firepit'::text THEN 'firepit'::text
                             WHEN planet_osm_polygon.amenity = 'bicycle_parking'::text THEN 'bicycle_parking'::text
-                            WHEN planet_osm_polygon.amenity = 'shelter'::text OR planet_osm_polygon.tourism = 'alpine_hut'::text OR planet_osm_polygon.highway = 'bus_stop'::text AND planet_osm_polygon.tags @> '"shelter"=>"yes"'::hstore THEN 'shelter'::text
+                            WHEN planet_osm_polygon.amenity = 'shelter'::text OR planet_osm_polygon.highway = 'bus_stop'::text AND planet_osm_polygon.tags @> '"shelter"=>"yes"'::hstore THEN 'shelter'::text
                             WHEN planet_osm_polygon.tourism = 'picnic_site'::text THEN 'picnic_site'::text
                             ELSE NULL::text
                         END AS type
@@ -514,7 +696,50 @@ CREATE INDEX bikemap_shelters_way_idx
     ON public.bikemap_shelters USING gist
     (way)
     TABLESPACE pg_default;
+
+-- View: public.bikemap_shops
+
+DROP MATERIALIZED VIEW IF EXISTS public.bikemap_shops;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS public.bikemap_shops
+TABLESPACE pg_default
+AS
+ SELECT row_number() OVER () AS id,
+    shops.center::geometry(Point,3857) AS center,
+    shops.shop
+   FROM ( SELECT st_centroid(unnest(st_clusterwithin(aaa.way, 3000::double precision))) AS center,
+            aaa.shop
+           FROM ( SELECT planet_osm_point.way,
+                    planet_osm_point.shop
+                   FROM planet_osm_point
+                  WHERE planet_osm_point.shop = ANY (ARRAY['bakery'::text, 'convenience'::text, 'deli'::text, 'departament_store'::text, 'supermarket'::text])
+                UNION
+                 SELECT st_centroid(planet_osm_line.way) AS st_centroid,
+                    planet_osm_line.shop
+                   FROM planet_osm_line
+                  WHERE planet_osm_line.shop = ANY (ARRAY['bakery'::text, 'convenience'::text, 'deli'::text, 'departament_store'::text, 'supermarket'::text])
+                UNION
+                 SELECT st_centroid(planet_osm_polygon.way) AS st_centroid,
+                    planet_osm_polygon.shop
+                   FROM planet_osm_polygon
+                  WHERE planet_osm_polygon.shop = ANY (ARRAY['bakery'::text, 'convenience'::text, 'deli'::text, 'departament_store'::text, 'supermarket'::text])) aaa
+          GROUP BY aaa.shop) shops
+WITH DATA;
+
+ALTER TABLE IF EXISTS public.bikemap_shops
+    OWNER TO postgres;
+
+
+CREATE INDEX bikemap_shops_center_idx
+    ON public.bikemap_shops USING gist
+    (center)
+    TABLESPACE pg_default;
+CREATE INDEX bikemap_shops_shop
+    ON public.bikemap_shops USING btree
+    (shop COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
 	
+
 -- View: public.bikemap_water
 
 DROP MATERIALIZED VIEW IF EXISTS public.bikemap_water;
@@ -543,7 +768,6 @@ WITH DATA;
 
 ALTER TABLE IF EXISTS public.bikemap_water
     OWNER TO postgres;
-
 
 CREATE INDEX bikemap_water_landuse_idx
     ON public.bikemap_water USING btree
